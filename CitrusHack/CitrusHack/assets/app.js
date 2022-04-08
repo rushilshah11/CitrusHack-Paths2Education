5024678297789a1ee74208d87bf3e1054a2a6770
rushilshah11 <89228996+rushilshah11@users.noreply.github.com>
2022-04-08 14:15:55 -0700

    Add files via upload

---
+++ CitrusHack/CitrusHack/assets/app.js
@@ -0,0 +1,20 @@
+const { default: Swiper } = require("swiper");
+
+const selectElement = (selector) => {
+    const element = document.querySelector(selector);
+    if(element) return element;
+    throw new Error(`Something went wrong! Make sure that ${selector} exists/is typed correctly.`);  
+};
+
+const menuToggleIcon = selectElement('#menu-toggle-icon');
+const formOpenBtn = selectElement('#search-icon');
+const formCloseBtn = selectElement('#form-close-btn');
+const searchContainer = selectElement('#search-form-container');
+
+const toggleMenu = () =>{
+    const mobileMenu = selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+}
+
+menuToggleIcon.addEventListener('click', toggleMenu);
